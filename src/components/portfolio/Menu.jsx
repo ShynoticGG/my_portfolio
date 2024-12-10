664e94f192b8b337c720c7e981876628b4137ae1
Skina Adonai <137023760+ShynoticGG@users.noreply.github.com>
2024-12-11 00:50:08 +0700

    Add files via upload

---
+++ src/components/portfolio/Menu.jsx
@@ -0,0 +1,37 @@
+import Work1 from "../../assets/screenshot.png";
+import Work2 from "../../assets/undangan_nikah.png";
+import Work3 from "../../assets/umkm.png"
+import Work4 from "../../assets/ecotambak.png"
+
+const Menu = [
+	{
+		id: 1,
+		image: Work1,
+		title: "Portfolio",
+		category: ["Frontend", "React"],
+		repositoryUrl: "https://github.com/SkinaAdonai/my_portfolio"
+	},
+	
+	{
+		id: 2,
+		image: Work2,
+		title: "Undangan Nikah",
+		category: ["Frontend"],
+		repositoryUrl: "https://github.com/SkinaAdonai/Yaki-Wedding"
+	},
+	{
+		id: 3,
+		image: Work3,
+		title: "Kasir UMKM",
+		category: ["Frontend", "Backend"],
+		repositoryUrl: "https://github.com/ShynoticGG/esteh_ibukota.git"
+	},{
+		id: 4,
+		image: Work4,
+		title: "EcoTambak",
+		category: ["Frontend", "Backend", "React"],
+		repositoryUrl: "https://github.com/LAUT-MERAH/massive_front-end.git"
+	},
+];
+
+export default Menu;
